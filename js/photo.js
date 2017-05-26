e9f6f8214b16bb83be12d6795b64e40b0da9718f
OrangeBorning <1550812270@qq.com>
2017-05-26 21:37:12 +0800

    Site updated: 2017-05-26 21:37:12

--- js/photo.js
+++ js/photo.js
@@ -1,47 +1,57 @@
+/**
+ * Created by zlw on 2016/1/2.
+ */
 define([], function () {
-  return {
-    page: 1,
-    offset: 20,
-    init: function () {
-      var that = this;
-      $.getJSON("/photo/output.json", function (data) {
-        that.render(that.page, data);
+    return {
+        page: 1,
+        offset: 20,
+        init: function () {
+            var that = this;
+            $.getJSON("/photo/output.json", function (data) {
+                that.render(that.page, data);
 
-        that.scroll(data);
-      });
-    },
+                that.scroll(data);
+            });
+        },
 
-    render: function (page, data) {
-      var begin = (page - 1) * this.offset;
-      var end = page * this.offset;
-      if (begin >= data.length) return;
-      var html, li = "";
-      for (var i = begin; i < end && i < data.length; i++) {
-        li += '<li><div class="img-box">' +
+        render: function (page, data) {
+            var begin = (page - 1) * this.offset;
+            var end = page * this.offset;
+            if (begin >= data.length) return;
+            var html, li = "";
+            for (var i = begin; i < end && i < data.length; i++) {
+                li += '<li><div class="img-box">' +
           '<a class="img-bg" rel="example_group" href="http://oqir7yp8o.bkt.clouddn.com/' + data[i] + '?raw=true"></a>' +
           '![](http://oqir7yp8o.bkt.clouddn.com/' + data[i] + '?imageView2/1/w/300/h/300/q/100&raw=true)' +
           '</li>';
-      }
+            }
+
+            //html = '<section class="archives album">' +
+            //    '<ul class="img-box-ul">' + li + '</ul>' +
+            //    '</section>';
 
 
-      $(".img-box-ul").append(li);
-      $(".img-box-ul").lazyload();
-      $("a[rel=example_group]").fancybox();
-    },
+            $(".img-box-ul").append(li);
+            $(".img-box-ul").lazyload();
+            $("a[rel=example_group]").fancybox();
+        },
 
-    scroll: function (data) {
-      var that = this;
-      $(window).scroll(function () {
-        var windowPageYOffset = window.pageYOffset;
-        var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
-        var sensitivity = 0;
+        scroll: function (data) {
+            var that = this;
+            $(window).scroll(function() {
+                var windowPageYOffset = window.pageYOffset;
+                var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
+                var sensitivity = 0;
 
-        var offsetTop = $(".instagram").offset().top + $(".instagram").height();
+                var offsetTop = $(".instagram").offset().top + $(".instagram").height();
 
-        if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
-          that.render(++that.page, data);
+                if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
+                    that.render(++that.page, data);
+                }
+            })
         }
-      })
     }
-  }
+
 })
+
+
